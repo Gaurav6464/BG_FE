eefd256903bbe2824d586b9c3cb0b1cb29be8c78
Gaurav Pardeshi <gaurav.pardeshi@galaxyweblinks.in>
2025-07-22 18:03:42 +0530

    remove Suspense

--- src/routes/Public.routes.tsx
+++ src/routes/Public.routes.tsx
@@ -1,5 +1,5 @@
 import { Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 
 // Lazy-loaded components
 const Signup = lazy(() => import("../pages/auth/Signup"));
@@ -7,22 +7,18 @@ const VerifyOtpOnSignup = lazy(() => import("../pages/auth/VerifyOtpSignup"));
 const Login = lazy(() => import("../pages/auth/Login"));
 const ForgetPassword = lazy(() => import("../pages/auth/ForgetPassword"));
 const VerifyOtpForget = lazy(() => import("../pages/auth/VerifyOtpForget"));
-const Loading = () => (
-  <div className="flex items-center justify-center h-screen w-full bg-black">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-white border-solid"></div>
-  </div>
-);
+
 
 
 const PublicRoutes = (
   <>
-    <Suspense fallback={<Loading />}>
+   
     <Route path="/signup" element={<Signup />} />
     <Route path="/verify-otp-signup" element={<VerifyOtpOnSignup />} />
     <Route path="/login" element={<Login />} />
     <Route path="/forgot" element={<ForgetPassword />} />
     <Route path="/verify-forget-password" element={<VerifyOtpForget />} />
-    </Suspense>
+
   </>
 );
 
